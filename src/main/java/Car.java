9aa6abfaab55bc12ebf0c07b1afe11872afa2075
Christian P <113365748+cperona@users.noreply.github.com>
2026-02-05 13:45:46 +0100

    feat: add brake() and accelerate() methods

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,25 @@
 public class Car {
+    //brand només es pot inicialitzar en la mateixa declaració perquè és static final
     public static final String brand = "";
-    public static String model = "";
-    public final int power = 0;
+    public static String model;
+    public final int power;
+
+    public Car(String model, int power) {
+        //Static final
+        //this.brand = "brando";
+
+        //Static
+        this.model = model;
+
+        //Final
+        this.power = power;
+    }
+
+    public static void brake() {
+        System.out.println("Vehicle is braking...");
+    }
+
+    public void accelerate() {
+        System.out.println("Vehicle is accelerating...");
+    }
 }
